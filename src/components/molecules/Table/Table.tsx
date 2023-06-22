8de9fbe6654774f104c8c205bdba8c49c0a4c48e
Gytis V <gytis.valatka@codeacademylt.onmicrosoft.com>
2023-06-22 13:13:58 +0300

    table sorting

--- src/components/molecules/Table/Table.tsx
+++ src/components/molecules/Table/Table.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DataTypeContext } from '../../../App';
 import { IIdentifiableItem, ITableField } from '../../../shared/types';
 import {
@@ -29,8 +29,69 @@ function TableRowItem({ fields, rowIndex }: ITableRowItemProps) {
   );
 }
 
+export interface ISort {
+  field: string | null;
+  direction: 'asc' | 'desc' | 'none';
+}
+
 export default function Table({ data }: { data: IIdentifiableItem[] }) {
+  console.log('rendering Table');
+
   const { getFieldsFunction } = useContext(DataTypeContext);
+
+  const [sort, setSort] = useState<ISort>({ field: null, direction: 'none' });
+
+  const toggleSortDirection = (
+    prev: ISort['direction']
+  ): ISort['direction'] => {
+    switch (prev) {
+      case 'none':
+        return 'asc';
+      case 'asc':
+        return 'desc';
+      case 'desc':
+        return 'none';
+      default:
+        return 'none';
+    }
+  };
+
+  const sortByField = (fieldName: string) => {
+    setSort((prev) => {
+      if (prev.field === fieldName) {
+        return {
+          field: prev.field,
+          direction: toggleSortDirection(prev.direction),
+        };
+      } else {
+        return { field: fieldName, direction: 'asc' };
+      }
+    });
+
+    //Sometimes you useEffect. Sometimes you don't. Why?
+  };
+
+  const toSorted = (
+    fieldName: string | null,
+    direction: ISort['direction'],
+    data: IIdentifiableItem[]
+  ): IIdentifiableItem[] => {
+    if (direction === 'none' || fieldName === null) {
+      return data;
+    }
+
+    const directionModifier = direction === 'asc' ? 1 : -1;
+
+    const fieldsDef = getFieldsFunction();
+    const fieldIndex = fieldsDef.findIndex((field) => field.slug === fieldName);
+    //toSorted not campatible with TypeScript yet
+    //need deep clone because react props are immutable
+    return data.toSorted((item1, item2) => {
+      const val1 = getFieldsFunction(item1)[fieldIndex].value;
+      const val2 = getFieldsFunction(item2)[fieldIndex].value;
+      return (val1 > val2 ? 1 : val1 < val2 ? -1 : 0) * directionModifier;
+    });
+  };
 
   return (
     <StyledTable>
@@ -40,12 +101,17 @@ export default function Table({ data }: { data: IIdentifiableItem[] }) {
             key={`header_${field.slug}`}
             $column={index + 1}
             $align={field.displayFormatter.align}
+            $sort={sort.field === field.slug ? sort.direction : 'none'}
+            $nextSortDirection={toggleSortDirection(
+              sort.field === field.slug ? sort.direction : 'none'
+            )}
+            onClick={() => sortByField(field.slug)}
           >
             {field.displayTitle}
           </StyledTableColHeader>
         ))}
       </StyledTableRowWrapper>
-      {data.map((item, index) => (
+      {toSorted(sort.field, sort.direction, data).map((item, index) => (
         <TableRowItem
           key={`row_${item.id}`}
           fields={getFieldsFunction(item)}
